61275623c83bf5d9380dc150eb73b243f7a2380f
kirio77 <45064317+kirio77@users.noreply.github.com>
2018-11-21 17:41:53 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -49,9 +49,9 @@
       }
       $.ajax({
         type: "POST",
-		charset: "utf-8",
        /* url: "https://docs.google.com/forms/d/1OCiSMSdit8QgxlhWjO9y9MmtFgpLdo52xf7UaCfY_ng/formResponse", */
         url: "https://docs.google.com/forms/d/e/1FAIpQLSe6Va_ZPMhsfEQZUET2skS2Oy-4eb6amG2XK1MyBt3-ykJeSw/formResponse",	   
+		contentType: "charset=utf-8",
         data: dataString,
         complete:sent
       });
