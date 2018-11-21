8fb7c7aaba34b82b896248f7ce919c23194cbcb7
kirio77 <45064317+kirio77@users.noreply.github.com>
2018-11-21 15:26:36 +0100

    Update mail.php

--- mail/mail.php
+++ mail/mail.php
@@ -1,12 +1,11 @@
 <?php
 if(!empty($_POST))
 {
- 
+header('Content-Type: text/html; charset=utf-8'); 
 $name = 'Name: '.$_POST[name]."\n";
 $email = 'E-mail: '.$_POST[email]."\n";
 $guest = "Guest's Name: ".$_POST[guest]."\n";
 $attending = '#Attending: '.$_POST[attending]."\n";
-
  
 $ip = getRealIpAddr();
 $message = "$name$email$guest$attending\n\nFrom page: ".urldecode($_SERVER['HTTP_REFERER'])."\nIP: $ip\nBrowser: ".$_SERVER['HTTP_USER_AGENT']."\n";
