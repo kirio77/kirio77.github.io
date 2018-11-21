1e655d14e17b19bca0a2224551b0518d0b8d422a
kirio77 <45064317+kirio77@users.noreply.github.com>
2018-11-21 18:11:38 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -50,9 +50,8 @@
       $.ajax({
         type: "POST",
        /* url: "https://docs.google.com/forms/d/1OCiSMSdit8QgxlhWjO9y9MmtFgpLdo52xf7UaCfY_ng/formResponse", */
-        var uri: "https://docs.google.com/forms/d/e/1FAIpQLSe6Va_ZPMhsfEQZUET2skS2Oy-4eb6amG2XK1MyBt3-ykJeSw/formResponse",	   
-        var res: encodeURIComponent(uri), 
-		contentType: "application/x-www-form-urlencoded;charset=utf-8",
+        url: "https://docs.google.com/forms/d/e/1FAIpQLSe6Va_ZPMhsfEQZUET2skS2Oy-4eb6amG2XK1MyBt3-ykJeSw/formResponse",	   
+        contentType: "application/x-www-form-urlencoded;charset=utf-8",
         data: dataString,
         complete:sent
       });
